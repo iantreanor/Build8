0e45ac7ae447e9728ea41af339bb63abb68b3d72
iantreanor <trei1_15@uni.worc.ac.uk>
2017-04-06 10:44:02 +0100

    hy

---
+++ www/brewery.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>
+Beers
+</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="http://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.css" />
+	<script src="http://code.jquery.com/jquery-1.12.1.min.js"></script>
+	<script src="http://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.js"></script>
+	<link rel="stylesheet" type="text/css" href="css/css.css">
+
+</head>
+<body>
+
+<br><br>
+<div data-role="header" data-position="fixed">
+	<h1>Beers of the Festival</h1>
+	<a href="javascript:history.back()" data-role="button" data-icon="back">Back</a>
+</div>
+
+<div data-role="main" class="ui-content">
+<p>In 2017 we currently host 48 brewersfrom all over the country.</p>
+<p>Take a look at them here.....</p>
+
+
+
+<ul data-role="listview" data-autodividers="true" data-filter="true">
+<li><a><h2>	Abbeydale	</h2><p>	Surrey	</p></a></li>
+<li><a><h2>	Acorn	</h2><p>	South Yorkshire	</p></a></li>
+<li><a><h2>	Adnams	</h2><p>	South Yorkshire	</p></a></li>
+<li><a><h2>	AJ's Ales	</h2><p>	London	</p></a></li>
+<li><a><h2>	Alechemy	</h2><p>	London	</p></a></li>
+<li><a><h2>	Anarchy	</h2><p>	Worcestershire	</p></a></li>
+<li><a><h2>	Angel Ales	</h2><p>	Hampshire	</p></a></li>
+<li><a><h2>	Arbor	</h2><p>	London	</p></a></li>
+<li><a><h2>	Arkell's	</h2><p>	Avon	</p></a></li>
+<li><a><h2>	Arundel	</h2><p>	Wiltshire	</p></a></li>
+<li><a><h2>	Ayr	</h2><p>	Buckinghamshire	</p></a></li>
+<li><a><h2>	Backyard Brewhouse Ltd	</h2><p>	West Midlands	</p></a></li>
+<li><a><h2>	Ballard's	</h2><p>	North Yorkshire	</p></a></li>
+<li><a><h2>	Barngates	</h2><p>	London	</p></a></li>
+<li><a><h2>	Battledown	</h2><p>	Avon & Somerset	</p></a></li>
+<li><a><h2>	Bewdley	</h2><p>	Gloucestershire	</p></a></li>
+<li><a><h2>	Black Hole Brewery	</h2><p>	Durham	</p></a></li>
+<li><a><h2>	Black Iris Brewery	</h2><p>	Staffordshire	</p></a></li>
+<li><a><h2>	Blue Bear Brewery	</h2><p>	Cornwall	</p></a></li>
+<li><a><h2>	Bradfield	</h2><p>	Wiltshire	</p></a></li>
+<li><a><h2>	Brains	</h2><p>	Wiltshire	</p></a></li>
+<li><a><h2>	Brampton Brewery	</h2><p>	Derbyshire	</p></a></li>
+<li><a><h2>	Brimstage	</h2><p>	Surrey	</p></a></li>
+<li><a><h2>	Brown Cow Alchemy	</h2><p>	London	</p></a></li>
+<li><a><h2>	Chantry	</h2><p>	Norfolk	</p></a></li>
+<li><a><h2>	Church End	</h2><p>	Manchester	</p></a></li>
+<li><a><h2>	Loch Lomond	</h2><p>	Cornwall	</p></a></li>
+<li><a><h2>	Newark	</h2><p>	Devon	</p></a></li>
+<li><a><h2>	North Cotswold	</h2><p>	Norfolk	</p></a></li>
+<li><a><h2>	Nottingham	</h2><p>	Devon	</p></a></li>
+<li><a><h2>	Oakham	</h2><p>	Devon	</p></a></li>
+<li><a><h2>	Ossett	</h2><p>	London	</p></a></li>
+<li><a><h2>	Penpont	</h2><p>	Yorkshire	</p></a></li>
+<li><a><h2>	Portobello	</h2><p>	Lincolnshire	</p></a></li>
+<li><a><h2>	Purple Moose	</h2><p>	Warwickshire	</p></a></li>
+<li><a><h2>	Quartz	</h2><p>	Somerset	</p></a></li>
+<li><a><h2>	Rat	</h2><p>	Kent	</p></a></li>
+<li><a><h2>	Sheffield	</h2><p>	Cornwall	</p></a></li>
+<li><a><h2>	Squawk	</h2><p>	Nottinghamshire	</p></a></li>
+
+</ul>
+</div>
+ 
+<div data-role="footer" data-position="fixed" data-id="myfooter">
+<div data-role="navbar">
+  <ul>
+  <li><a href="index.html" class="ui.btn-active"><img src="img/home.png" width="60%" height="60%"></a></li>
+  <li><a href="beers.html" class="ui.btn-active"><img src="img/beermug3.png" width="60%" height="60%"></a></li>
+  <li><a href="festival.html" class="ui.btn-active"><img src="img/festival2.png" width="60%" height="60%"></a></li>
+  <li><a href="brewery.html" class="ui.btn-active"><img src="img/brewery3.png" width="60%" height="60%"></a></li>
+  <li><a href="camera.html" class="ui.btn-active"><img src="img/camera.png" width="100%" height="100%"></a></li>
+</ul>
+</div>
+</div>
+  </body>
+</html>
