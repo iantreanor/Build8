42a62b24038a181d516a157f1f60a53594203d47
iantreanor <trei1_15@uni.worc.ac.uk>
2017-04-06 08:59:02 +0100

    a

---
+++ www/worcester/worcesterhome.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>
+Beers
+</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="http://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.css" />
+	<script src="http://code.jquery.com/jquery-1.12.1.min.js"></script>
+	<script src="http://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.js"></script>
+	<link rel="stylesheet" type="text/css" href="../css/css.css">
+
+</head>
+<body>
+<div data-role="header" data-position="fixed">
+	<h1>Worcester beer and Cider Festival</h1>
+	<a href="javascript:history.back()" data-role="button" data-icon="back">Back</a>
+</div>
+
+<img src="../img/worcbeerfest.jpg" height="50%" width="80%";>
+
+<ul data-role="listview">  
+<li><a href="worcesterbeers.html" class="ui-btn ui-icon-home ui-btn-icon-left">BEERS</a><span class="ui-icon ui-icon-arrowthick-1-n"></span></li>
+
+<li><a href="brewery.html" class="ui-btn ui-icon-eye ui-btn-icon-left">BREWERYS</a><span class="ui-icon ui-icon-arrowthick-1-n"></span></li>
+
+<li><a href="worcesterlocation.html" class="ui-btn ui-icon-location ui-btn-icon-left">LOCATION MAP</a><span class="ui-icon ui-icon-arrowthick-1-n"></span></li>
+
+<li><a href="weather.html" class="ui-btn ui-icon-cloud ui-btn-icon-left">WEATHER FORCAST</a><span class="ui-icon ui-icon-arrowthick-1-n"></span></li>
+
+<li><a href="camera.html" class="ui-btn ui-icon-camera ui-btn-icon-left">CAMERA</a><span class="ui-icon ui-icon-arrowthick-1-n"></span></li>
+
+</ul>
+
+<div data-role="footer" data-position="fixed">
+<div data-role="navbar">
+  <ul>
+  <li><a href="../index.html" class="ui.btn-active"><img src="../img/home.png" width="100%" height="100%"></a></li>
+  <li><a href="../beers.html" class="ui.btn-active"><img src="../img/beermug3.png" width="100%" height="100%"></a></li>
+  <li><a href="../festival.html" class="ui.btn-active"><img src="../img/festival2.png" width="100%" height="100%"></a></li>
+  <li><a href="../brewery.html" class="ui.btn-active"><img src="../img/brewery3.png" width="100%" height="100%"></a></li>
+  <li><a href="../camera.html" class="ui.btn-active"><img src="../img/camera.png" width="100%" height="100%"></a></li>
+
+</div>
+</div>
+
+  
+  </body>
+</html>
